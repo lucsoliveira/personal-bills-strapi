18ec4064188a2f16af86e32788e0d7a59d57a2a0
Lucas Oliveira <olucas1998@gmail.com>
2024-05-18 15:27:18 -0300

    refactor: api filter

--- apps/financial-dashboard/src/pages/dashboard/components/Navigation/Navigation.tsx
+++ apps/financial-dashboard/src/pages/dashboard/components/Navigation/Navigation.tsx
@@ -13,9 +13,9 @@ import {
   iDataPendingPayments,
 } from '../../../../mock/dashboard';
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Spinner } from '../../../../components/Spinner';
 import { Transaction } from '../../../../types/transactions';
+import { TRANSACTIONS_SERVICES } from '../../../../services/api';
 
 const getIcon = (type: string) => {
   switch (type) {
@@ -70,51 +70,10 @@ const RecentTransactions = () => {
   );
 
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const fetchTransactions = async ({
-    pageSize,
-    sortBy,
-  }: {
-    pageSize: number;
-    sortBy?: string[];
-  }) => {
-    const sort = sortBy
-      ? {
-          sort: {
-            ...sortBy,
-          },
-        }
-      : {};
-
-    const results = await axios.request({
-      baseURL: 'http://localhost:1337/api/transactions?populate=*',
-      params: {
-        pagination: {
-          pageSize,
-        },
-        ...sort,
-      },
-      headers: {
-        Authorization: `Bearer ${'30373287118fd27df9a94adc4d6066cac568afdb7d7b082d282c6e40cd500ba106c43e7a174513daa1ec4d21d8bb3199ad70ef68e96072cfa1364260b566208ee9bca1a79da0ab123effdbb43a4caf0bb1afa6786c8d183b272a10948e58ab1085809f1071f47c97f655b4106bedf18f54b6bda83e01fbf8bb91fbc33f5108f7'}`,
-      },
-    });
-
-    const data = results.data.data;
-    const dataNormalized = data.map(
-      (item: { id: number; attributes: Transaction }) => {
-        return {
-          id: item.id,
-          ...item.attributes,
-        };
-      }
-    );
-    return {
-      transactions: dataNormalized,
-    };
-  };
 
   useEffect(() => {
     setIsLoading(true);
-    fetchTransactions({
+    TRANSACTIONS_SERVICES.filter({
       pageSize: 4,
       sortBy: ['date:desc'],
     })
--- apps/financial-dashboard/src/services/api.ts
+++ apps/financial-dashboard/src/services/api.ts
@@ -1,5 +1,15 @@
 import axios from 'axios';
+import { TransactionsServices } from './transactions';
 
 export const api = axios.create({
   baseURL: import.meta.env.API_URL,
 });
+
+export const apiClient = axios.create({
+  baseURL: 'http://localhost:1337/api',
+  headers: {
+    Authorization: `Bearer ${import.meta.env.TOKEN_API}`,
+  },
+});
+
+export const TRANSACTIONS_SERVICES = TransactionsServices(apiClient);
